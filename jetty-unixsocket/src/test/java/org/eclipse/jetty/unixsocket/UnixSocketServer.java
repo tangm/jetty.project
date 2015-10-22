67b210b9ea81af68ce3848a114bbbd1b80a8ca52
Greg Wilkins <gregw@webtide.com>
2015-10-22 23:48:57 +1100

    working with haproxy

--- jetty-unixsocket/src/test/java/org/eclipse/jetty/unixsocket/UnixSocketServer.java
+++ jetty-unixsocket/src/test/java/org/eclipse/jetty/unixsocket/UnixSocketServer.java
@@ -1,6 +1,14 @@
 package org.eclipse.jetty.unixsocket;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.eclipse.jetty.server.Request;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.AbstractHandler;
 
 public class UnixSocketServer
 {
@@ -10,6 +18,20 @@ public class UnixSocketServer
         
         UnixSocketConnector connector = new UnixSocketConnector(server);
         server.addConnector(connector);
+        
+        server.setHandler(new AbstractHandler()
+        {
+
+            @Override
+            protected void doHandle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
+                    throws IOException, ServletException
+            {
+                baseRequest.setHandled(true);
+                response.setStatus(200);
+                response.getWriter().write("Hello World");
+            }
+            
+        });
         
         server.start();
         server.join();
